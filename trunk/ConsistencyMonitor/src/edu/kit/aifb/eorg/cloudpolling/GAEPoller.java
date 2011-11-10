0b55685affb2b497d7ee91762161c54be6b501d0
rhh.hoffmann@gmail.com <rhh.hoffmann@gmail.com@ddf85333-b1a3-67e3-8036-f7410c508340>
2011-11-10 16:54:16 +0000

    GAEPoller hinzugefügt

---
+++ trunk/ConsistencyMonitor/src/edu/kit/aifb/eorg/cloudpolling/GAEPoller.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package edu.kit.aifb.eorg.cloudpolling;
+
+import edu.kit.aifb.eorg.connectors.GAEConnector;
+
+/**
+ * @author Robin Hoffmann
+ * 
+ *         created on: 10.11.2011
+ */
+public class GAEPoller extends AbstractPoller {
+
+	
+	@Override
+	public String readFromCloud(String key) {
+		return GAEConnector.readFromGAE(key);
+	}
+	
+	@Override
+	public void configure(String[] args) throws Exception {
+		if (args[0]==null) {
+			log.error("Missing parameters: GAE Url");
+			throw new Exception(
+					"Missing parameters: GAE Url");
+		}
+		GAEConnector.doInitialize(args[0]);
+	}
+
+}
